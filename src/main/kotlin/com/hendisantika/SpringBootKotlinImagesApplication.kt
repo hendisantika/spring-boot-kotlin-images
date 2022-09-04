35bedb8fcd3e08fc51c7b72b1fdd6fafc21a96fc
Hendi Santika <hendisantika@yahoo.co.id>
2022-09-04 11:02:20 +0700

    #9 - Set cors in Main App :green_book: :penguin:

--- src/main/kotlin/com/hendisantika/SpringBootKotlinImagesApplication.kt
+++ src/main/kotlin/com/hendisantika/SpringBootKotlinImagesApplication.kt
@@ -2,9 +2,24 @@ package com.hendisantika
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
-class SpringBootKotlinImagesApplication
+class SpringBootKotlinImagesApplication {
+    @Bean
+    fun cors(): WebMvcConfigurer {
+
+        return object : WebMvcConfigurer {
+
+            override fun addCorsMappings(registry: CorsRegistry) {
+
+                registry.addMapping("/**").allowedOrigins("http://localhost:63343")
+            }
+        }
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<SpringBootKotlinImagesApplication>(*args)
