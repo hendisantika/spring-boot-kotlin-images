64bd09541d19a6f59daeef6a0641ba6066f9b46b
Hendi Santika <hendisantika@yahoo.co.id>
2022-09-04 11:03:46 +0700

    #11 - Add setProfilePicture API in UserController :green_book: :penguin:

--- src/main/kotlin/com/hendisantika/controller/UserController.kt
+++ src/main/kotlin/com/hendisantika/controller/UserController.kt
@@ -1,7 +1,14 @@
 package com.hendisantika.controller
 
 import com.hendisantika.service.UserService
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
+import java.net.URI
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,4 +21,20 @@ import org.springframework.web.bind.annotation.RestController
  * To change this template use File | Settings | File Templates.
  */
 @RestController
-class UserController(private val userService: UserService)
+class UserController(private val userService: UserService) {
+    @PostMapping(value = ["/api/user/{userId}/profile-picture"], consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
+    fun setProfilePicture(@PathVariable("userId") id: Long, @RequestParam file: MultipartFile): ResponseEntity<Void> {
+
+        return try {
+            userService.setProfilePicture(id, file)
+            ResponseEntity
+                .created(URI("/api/user/${id}/profile-picture"))
+                .build()
+        } catch (error: NoSuchElementException) {
+            ResponseEntity
+                .notFound()
+                .build()
+        }
+
+    }
+}
