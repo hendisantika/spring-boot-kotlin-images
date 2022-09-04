f759bd52f1522db883ff930dec894dea12371078
Hendi Santika <hendisantika@yahoo.co.id>
2022-09-04 11:00:47 +0700

    #7 - Add setProfilePicture in UserService :green_book: :penguin:

--- src/main/kotlin/com/hendisantika/service/UserService.kt
+++ src/main/kotlin/com/hendisantika/service/UserService.kt
@@ -1,7 +1,9 @@
 package com.hendisantika.service
 
+import com.hendisantika.entity.User
 import com.hendisantika.repository.UserRepository
 import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,4 +16,12 @@ import org.springframework.stereotype.Service
  * To change this template use File | Settings | File Templates.
  */
 @Service
-class UserService(private val userRepository: UserRepository)
+class UserService(private val userRepository: UserRepository) {
+
+    fun setProfilePicture(id: Long, file: MultipartFile) {
+        val user: User = userRepository.findById(id).orElseThrow()
+        user.profilePicture = file.bytes
+        userRepository.save(user)
+    }
+
+}
