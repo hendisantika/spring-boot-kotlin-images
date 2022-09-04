0498cd845e4bfcd51826ba50f279e8b6ccb4c0a4
Hendi Santika <hendisantika@yahoo.co.id>
2022-09-04 10:51:57 +0700

    #1 - Initial commit :bowtie: :whale:

---
+++ src/main/kotlin/com/hendxiasntika/SpringBootKotlinImagesApplication.kt
@@ -0,0 +1,11 @@
+package com.hendxiasntika
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootKotlinImagesApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootKotlinImagesApplication>(*args)
+}
---
+++ src/test/kotlin/com/hendxiasntika/SpringBootKotlinImagesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hendxiasntika
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootKotlinImagesApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
